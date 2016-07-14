cb1dbb2fe8568b93f0a4251f6d01cbc262795675
Katya Zdorov and Paul Zabelin <katyaskvo+katya+paul@gmail.com>
2016-07-14 13:47:42 -0700

    make animation repeatable

--- transport app/TractorViewController.swift
+++ transport app/TractorViewController.swift
@@ -5,13 +5,14 @@ class TractorViewController: UIViewController {
     @IBOutlet var smallWheelView: UIImageView!
 
     @IBAction func startAnimation() {
-        UIView.animateWithDuration(1,
-                                   delay: 1,
-                                   options: [.Repeat, .CurveLinear],
+        bigWheelView.transform = CGAffineTransformIdentity
+        smallWheelView.transform = CGAffineTransformIdentity
+        UIView.animateWithDuration(0.75,
+                                   delay: 0,
+                                   options: [.CurveLinear],
                                    animations: {
                                     self.bigWheelView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
                                     self.smallWheelView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
             }, completion: nil)
-
     }
 }
