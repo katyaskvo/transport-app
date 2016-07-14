678dbafe5d2473a08b7b06fbb5bf043cc7f30c67
Katya Zdorov and Paul Zabelin <katyaskvo+katya+paul@gmail.com>
2016-07-14 13:43:16 -0700

    start animation on button tap

--- transport app/TractorViewController.swift
+++ transport app/TractorViewController.swift
@@ -4,7 +4,7 @@ class TractorViewController: UIViewController {
     @IBOutlet var bigWheelView: UIImageView!
     @IBOutlet var smallWheelView: UIImageView!
 
-    func startRotatingWheels() {
+    @IBAction func startAnimation() {
         UIView.animateWithDuration(1,
                                    delay: 1,
                                    options: [.Repeat, .CurveLinear],
@@ -12,14 +12,6 @@ class TractorViewController: UIViewController {
                                     self.bigWheelView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
                                     self.smallWheelView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
             }, completion: nil)
-    }
 
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        startRotatingWheels()
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
     }
 }
