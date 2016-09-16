abdfa7fd9b4c3950b2ab8a869a88ab847f0cbe04
Katya Zdorov and Paul Zabelin <katyaskvo+katya+paul@gmail.com>
2016-09-16 11:20:29 -0700

    rotate tractor wheels with different speed multiple times

--- transport app/TractorViewController.swift
+++ transport app/TractorViewController.swift
@@ -7,12 +7,13 @@ class TractorViewController: UIViewController {
     @IBAction func startAnimation() {
         bigWheelView.transform = CGAffineTransform.identity
         smallWheelView.transform = CGAffineTransform.identity
-        UIView.animate(withDuration: 0.75,
-                                   delay: 0,
-                                   options: [.curveLinear],
-                                   animations: {
-                                    self.bigWheelView.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI))
-                                    self.smallWheelView.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI))
+        UIView.animate(withDuration: 5,
+                       delay: 0,
+                       options: [.curveEaseIn],
+                       animations: {
+                        UIView.setAnimationRepeatCount(2)
+                        self.bigWheelView.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI * 2 / 3))
+                        self.smallWheelView.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI))
             }, completion: nil)
     }
 }
