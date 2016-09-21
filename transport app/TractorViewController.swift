5eb65b186bd5bd530f56aa96be86d16d6f820bf3
Ekaterina Zdorov <Skvo@Murzik-2.local>
2016-09-20 20:33:20 -0700

    tune up weel speed and rotation

--- transport app/TractorViewController.swift
+++ transport app/TractorViewController.swift
@@ -4,6 +4,7 @@ class TractorViewController: UIViewController {
     @IBOutlet var bigWheelView: UIImageView!
     @IBOutlet var smallWheelView: UIImageView!
     @IBOutlet var cloudView: UIImageView!
+    
 
     @IBAction func startAnimation() {
         bigWheelView.transform = CGAffineTransform.identity
@@ -12,16 +13,19 @@ class TractorViewController: UIViewController {
         cloudView.alpha = 1
         UIView.animate(withDuration: 2,
                        delay: 0,
-                       options: [.curveEaseIn],
+                       options: [.curveLinear],
                        animations: {
                         UIView.setAnimationRepeatCount(5)
                         self.cloudView.transform = CGAffineTransform(translationX: 100, y: -100).scaledBy(x: 5, y: 5)
 
                         self.cloudView.alpha = 0
-                        UIView.setAnimationDuration(4)
+                        UIView.setAnimationDuration(5)
                         UIView.setAnimationRepeatCount(3)
                         self.bigWheelView.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI * 2 / 3))
-                        self.smallWheelView.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI))
+                        UIView.setAnimationDuration(3)
+                        UIView.setAnimationRepeatCount(5)
+                        self.smallWheelView.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI * 2 / 3))
+                       
             }, completion: nil)
     }
 }
