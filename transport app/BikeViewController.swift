4cf142ce7c748efc5b943280cf7c6e345c416c51
Katya Zdorov <skvoev@gmail.com>
2016-09-27 11:21:19 -0700

    add bike view

---
+++ transport app/BikeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BikeViewController.swift
+//  transport app
+//
+//  Created by Ekaterina Zdorov on 7/13/16.
+//  Copyright © 2016 Ekaterina Zdorov. All rights reserved.
+//
+
+import UIKit
+
+class BikeViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
