3897526862d5b0b228ef6bb20ebe4536459bca8d
Katya Zdorov <skvoev@gmail.com>
2016-11-29 14:48:12 +0200

    add tractor wheel reflection animation

--- transport app/TractorViewController.swift
+++ transport app/TractorViewController.swift
@@ -5,7 +5,11 @@ class TractorViewController: UIViewController {
     @IBOutlet var bigWheelView: UIImageView!
     @IBOutlet var smallWheelView: UIImageView!
     @IBOutlet var cloudView: UIImageView!
+    @IBOutlet var reflectionBackWheelView: UIImageView!
+    @IBOutlet var reflectionFrontWheelView: UIImageView!
+    
     var soundId : SystemSoundID = 0
+    let animationDuration = CFTimeInterval(10.0)
 
     func playSound() {
         if soundId == 0 {
@@ -35,8 +39,10 @@ class TractorViewController: UIViewController {
                         self.cloudView.transform = CGAffineTransform(translationX: 100, y: -100).scaledBy(x: 5, y: 5)
 
                         self.cloudView.alpha = 0
-                        self.smallWheelView.rotate360Degrees(duration: 3.33, repeatCount: 3)
+                        self.smallWheelView.rotate360Degrees(duration: 4, repeatCount: 2.5)
                         self.bigWheelView.rotate360Degrees(duration: 5, repeatCount: 2)
+                        self.reflectionBackWheelView.animateWheelReflection(values: [0, M_PI * 0.04, 0, 0, M_PI * (-0.06), 0 ], keyTimes: [0, 0.15, 0.35, 0.65, 0.85, 1], animatedImageView: self.reflectionBackWheelView, duration: 0.8, animationDuration: self.animationDuration)
+                        self.reflectionFrontWheelView.animateWheelReflection(values: [0, M_PI * 0.04, 0, 0, M_PI * (-0.06), 0 ], keyTimes: [0, 0.15, 0.35, 0.65, 0.85, 1], animatedImageView: self.reflectionFrontWheelView, duration: 0.8, animationDuration: self.animationDuration)
                        
             }, completion: nil)
     }
