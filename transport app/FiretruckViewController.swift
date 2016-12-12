1e12ddf15737b124151c09ec2a6c3a530e5fafc8
Katya Zdorov <katyaskvo@gmail.com>
2016-12-12 16:32:42 +0200

    add wheels animation for firetruck

--- transport app/FiretruckViewController.swift
+++ transport app/FiretruckViewController.swift
@@ -4,23 +4,23 @@ class FiretruckViewController: UIViewController {
     @IBOutlet var frontWheelView: UIImageView!
     @IBOutlet var backWheelView: UIImageView!
     @IBOutlet var syrenView: UIImageView!
-    @IBOutlet var syrenShineView: UIImageView!
+//    @IBOutlet var syrenShineView: UIImageView!
     @IBAction func startAnimation() {
         frontWheelView.transform = CGAffineTransform.identity
         backWheelView.transform = CGAffineTransform.identity
-        syrenShineView.transform = CGAffineTransform.identity
-        syrenShineView.alpha = 1
+//        syrenShineView.transform = CGAffineTransform.identity
+//        syrenShineView.alpha = 1
         UIView.animate(withDuration: 1,
                        delay: 0,
                        options: [.curveLinear],
                        animations: {
                         UIView.setAnimationRepeatCount(15)
 
-                        self.syrenShineView.alpha = 0
+//                        self.syrenShineView.alpha = 0
                         UIView.setAnimationDuration(0.4)
                         UIView.setAnimationRepeatCount(30)
-                        self.frontWheelView.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI * 2 / 5))
-                        self.backWheelView.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI * 2 / 5))
-            }, completion: nil)
+                        self.frontWheelView.rotate360Degrees(duration: 1, repeatCount: 10)
+                        self.backWheelView.rotate360Degrees(duration: 1, repeatCount: 10)
+        }, completion: nil)
     }
 }
