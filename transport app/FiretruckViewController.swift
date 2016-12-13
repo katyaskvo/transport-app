db37975a1bb9fdc4922b5a469a24dfad49741489
Katya Zdorov <katyaskvo@gmail.com>
2016-12-13 14:45:47 +0200

    add body shake animation to the Firetruck View

--- transport app/FiretruckViewController.swift
+++ transport app/FiretruckViewController.swift
@@ -1,12 +1,21 @@
 import UIKit
 
 class FiretruckViewController: UIViewController {
+    @IBOutlet var firetruckBodyView: UIImageView!
     @IBOutlet var frontWheelView: UIImageView!
     @IBOutlet var backWheelView: UIImageView!
-    @IBOutlet var syrenView: UIImageView!
     @IBOutlet var roadBoxView: UIView!
     @IBOutlet var road1View: UIImageView!
     @IBOutlet var road2View: UIImageView!
+    
+    @IBOutlet var firetruckHoldersView: UIImageView!
+    @IBOutlet var light1View: UIImageView!
+    @IBOutlet var light2View: UIImageView!
+    @IBOutlet var light3View: UIImageView!
+    @IBOutlet var light4View: UIImageView!
+    @IBOutlet var light5View: UIImageView!
+    @IBOutlet var syrenView: UIImageView!
+    
     
     @IBOutlet var reflectionBackWheelView: UIImageView!
     @IBOutlet var reflectionFrontWheelView: UIImageView!
@@ -41,6 +50,18 @@ class FiretruckViewController: UIViewController {
                         //Wheel Reflection
                         self.reflectionBackWheelView.animateWheelReflection(values: [M_PI * 0.015, M_PI * (-0.02), M_PI * 0.015 ], keyTimes: [0, 0.5, 1], animatedImageView: self.reflectionBackWheelView, duration: 0.3, animationDuration: self.animationDuration)
                         self.reflectionFrontWheelView.animateWheelReflection(values: [M_PI * 0.015, M_PI * (-0.02), M_PI * 0.015 ], keyTimes: [0, 0.5, 1], animatedImageView: self.reflectionFrontWheelView, duration: 0.3, animationDuration: self.animationDuration)
+                        
+                        //Shake
+                        self.firetruckBodyView.shake(values: [1, -1, 1, -1, 1], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.firetruckBodyView, duration: 0.7, animationDuration: self.animationDuration)
+                        self.firetruckHoldersView.shake(values: [1, -1, 1, -1, 1], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.firetruckHoldersView, duration: 0.7, animationDuration: self.animationDuration)
+                        self.light5View.shake(values: [1, -1, 1, -1, 1], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.light5View, duration: 0.7, animationDuration: self.animationDuration)
+                        self.light4View.shake(values: [1, -1, 1, -1, 1], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.light4View, duration: 0.7, animationDuration: self.animationDuration)
+                        self.light3View.shake(values: [1, -1, 1, -1, 1], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.light3View, duration: 0.7, animationDuration: self.animationDuration)
+                        self.light2View.shake(values: [1, -1, 1, -1, 1], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.light2View, duration: 0.7, animationDuration: self.animationDuration)
+                        self.light1View.shake(values: [1, -1, 1, -1, 1], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.light1View, duration: 0.7, animationDuration: self.animationDuration)
+                        self.syrenView.shake(values: [1, -1, 1, -1, 1], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.syrenView, duration: 0.7, animationDuration: self.animationDuration)
+                        
+                        
         }, completion: nil)
     }
 }
