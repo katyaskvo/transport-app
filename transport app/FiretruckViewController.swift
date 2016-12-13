743584a8ed5c6ac05093c4c6d75315aa56e5811d
Katya Zdorov <katyaskvo@gmail.com>
2016-12-13 14:13:38 +0200

    add wheel reflection animation to Firetruck View

--- transport app/FiretruckViewController.swift
+++ transport app/FiretruckViewController.swift
@@ -7,14 +7,19 @@ class FiretruckViewController: UIViewController {
     @IBOutlet var roadBoxView: UIView!
     @IBOutlet var road1View: UIImageView!
     @IBOutlet var road2View: UIImageView!
-//    @IBOutlet var syrenShineView: UIImageView!
     
+    @IBOutlet var reflectionBackWheelView: UIImageView!
+    @IBOutlet var reflectionFrontWheelView: UIImageView!
     
     let animationDuration = CFTimeInterval(10.0)
     
     @IBAction func startAnimation() {
         frontWheelView.transform = CGAffineTransform.identity
         backWheelView.transform = CGAffineTransform.identity
+        
+        
+        reflectionBackWheelView.transform = CGAffineTransform.identity
+        reflectionFrontWheelView.transform = CGAffineTransform.identity
         
         road1View.transform = CGAffineTransform.identity
         road2View.transform = CGAffineTransform.identity
@@ -32,6 +37,10 @@ class FiretruckViewController: UIViewController {
                         self.frontWheelView.rotate360Degrees(duration: 1, repeatCount: 10)
                         self.backWheelView.rotate360Degrees(duration: 1, repeatCount: 10)
                         self.roadBoxView.moveTheRoad(road1View: self.road1View, road2View: self.road2View, duration: 1, animationDuration: self.animationDuration)
+                        
+                        //Wheel Reflection
+                        self.reflectionBackWheelView.animateWheelReflection(values: [M_PI * 0.015, M_PI * (-0.02), M_PI * 0.015 ], keyTimes: [0, 0.5, 1], animatedImageView: self.reflectionBackWheelView, duration: 0.3, animationDuration: self.animationDuration)
+                        self.reflectionFrontWheelView.animateWheelReflection(values: [M_PI * 0.015, M_PI * (-0.02), M_PI * 0.015 ], keyTimes: [0, 0.5, 1], animatedImageView: self.reflectionFrontWheelView, duration: 0.3, animationDuration: self.animationDuration)
         }, completion: nil)
     }
 }
