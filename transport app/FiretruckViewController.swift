449a0bac0df36ed4f89550dbd13af6a7b2adbf9d
Katya Zdorov <katyaskvo@gmail.com>
2016-12-13 13:58:53 +0200

    add road animation to the firetruck view

--- transport app/FiretruckViewController.swift
+++ transport app/FiretruckViewController.swift
@@ -4,10 +4,20 @@ class FiretruckViewController: UIViewController {
     @IBOutlet var frontWheelView: UIImageView!
     @IBOutlet var backWheelView: UIImageView!
     @IBOutlet var syrenView: UIImageView!
+    @IBOutlet var roadBoxView: UIView!
+    @IBOutlet var road1View: UIImageView!
+    @IBOutlet var road2View: UIImageView!
 //    @IBOutlet var syrenShineView: UIImageView!
+    
+    
+    let animationDuration = CFTimeInterval(10.0)
+    
     @IBAction func startAnimation() {
         frontWheelView.transform = CGAffineTransform.identity
         backWheelView.transform = CGAffineTransform.identity
+        
+        road1View.transform = CGAffineTransform.identity
+        road2View.transform = CGAffineTransform.identity
 //        syrenShineView.transform = CGAffineTransform.identity
 //        syrenShineView.alpha = 1
         UIView.animate(withDuration: 1,
@@ -21,6 +31,7 @@ class FiretruckViewController: UIViewController {
                         UIView.setAnimationRepeatCount(30)
                         self.frontWheelView.rotate360Degrees(duration: 1, repeatCount: 10)
                         self.backWheelView.rotate360Degrees(duration: 1, repeatCount: 10)
+                        self.roadBoxView.moveTheRoad(road1View: self.road1View, road2View: self.road2View, duration: 1, animationDuration: self.animationDuration)
         }, completion: nil)
     }
 }
