2d75e4e60f59761d1ab1f241c55d473692b3499d
Katya Zdorov <katyaskvo@gmail.com>
2017-02-07 13:38:28 +0700

    add headlights animation to the Beetle view

--- transport app/BeetleViewController.swift
+++ transport app/BeetleViewController.swift
@@ -11,11 +11,14 @@ class BeetleViewController: UIViewController {
     @IBOutlet var frontWheelReflectionView: UIImageView!
     @IBOutlet var windowReflectionView: UIImageView!
     @IBOutlet var pipeView: UIImageView!
+    @IBOutlet var headlightsView: UIImageView!
     @IBOutlet var roadBoxView: UIView!
-    @IBOutlet weak var road1View: UIImageView!
-    @IBOutlet weak var road2View: UIImageView!
+    @IBOutlet var road1View: UIImageView!
+    @IBOutlet var road2View: UIImageView!
     
     let animationDuration = CFTimeInterval(10.0)
+    
+    var headlightsImages: [UIImage]!
     
     func enableButton() {
         self.buttonPlay.isEnabled = true
@@ -34,10 +37,24 @@ class BeetleViewController: UIViewController {
         }
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let headlights_01 = UIImage(named: "headlights01")
+        let headlights_02 = UIImage(named: "headlights02")
+        headlightsImages = [headlights_01!, headlights_02!, headlights_02!, headlights_01!, headlights_02!, headlights_01!, headlights_02!, headlights_01!, headlights_01!]
+    }
+    
     @IBAction func startAnimation() {
         self.buttonPlay.isEnabled = false
         Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(enableButton), userInfo: nil, repeats: false)
         playSound()
+        
+        let headlightsAnimation = CAKeyframeAnimation(keyPath: "contents")
+        headlightsAnimation.calculationMode = kCAAnimationDiscrete
+        headlightsAnimation.values = headlightsImages.map {$0.cgImage as AnyObject}
+        headlightsAnimation.duration = 0.5
+        headlightsAnimation.repeatCount = 20
+
         
         UIView.animate(withDuration: 4,
                        delay: 0,
@@ -46,6 +63,7 @@ class BeetleViewController: UIViewController {
                         
                         //Shake
                         self.bodyView.shake(values: [0.75, -0.75, 0.75, -0.75, 0.75], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.bodyView, duration: 0.75, animationDuration: self.animationDuration)
+                        self.headlightsView.shake(values: [0.75, -0.75, 0.75, -0.75, 0.75], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.headlightsView, duration: 0.75, animationDuration: self.animationDuration)
                         self.windowReflectionView.shake(values: [0.25, -0.25, 0.25, -0.25, 0.25], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.windowReflectionView, duration: 0.75, animationDuration: self.animationDuration)
                         self.pipeView.shake(values: [0.5, -0.5, 0.5, -0.5, 0.5], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.pipeView, duration: 0.75, animationDuration: self.animationDuration)
                         self.backWheelView.shake(values: [0, 1, 0, 1.5, 0], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.backWheelView, duration: 0.75, animationDuration: self.animationDuration)
@@ -59,6 +77,9 @@ class BeetleViewController: UIViewController {
                         self.frontWheelView.rotate360Degrees(duration: 1, repeatCount: 10)
                         self.backWheelReflectionView.animateWheelReflection(values: [0, M_PI * 0.02, 0, M_PI * (-0.03), 0 ], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.backWheelReflectionView, duration: 0.5, animationDuration: self.animationDuration)
                         self.frontWheelReflectionView.animateWheelReflection(values: [0, M_PI * 0.02, 0, M_PI * (-0.03), 0 ], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.frontWheelReflectionView, duration: 0.5, animationDuration: self.animationDuration)
+                        
+                        //Headlights Shine
+                        self.headlightsView.layer.add(headlightsAnimation, forKey: "contents")
                         
                         //Road
                         self.roadBoxView.moveTheRoad(road1View: self.road1View, road2View: self.road2View, duration: 1, animationDuration: self.animationDuration)
