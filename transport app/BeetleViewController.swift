84e6f9593573f592847edcc659e7cd593d0d80ab
Katya Zdorov <katyaskvo@gmail.com>
2017-02-08 14:22:54 +0700

    add beetle exhaust pipe's draft animation

--- transport app/BeetleViewController.swift
+++ transport app/BeetleViewController.swift
@@ -15,10 +15,12 @@ class BeetleViewController: UIViewController {
     @IBOutlet var roadBoxView: UIView!
     @IBOutlet var road1View: UIImageView!
     @IBOutlet var road2View: UIImageView!
+    @IBOutlet var steamView: UIImageView!
     
     let animationDuration = CFTimeInterval(10.0)
     
     var headlightsImages: [UIImage]!
+    var steamImages: [UIImage]!
     
     func enableButton() {
         self.buttonPlay.isEnabled = true
@@ -42,6 +44,11 @@ class BeetleViewController: UIViewController {
         let headlights_01 = UIImage(named: "headlights01")
         let headlights_02 = UIImage(named: "headlights02")
         headlightsImages = [headlights_01!, headlights_02!, headlights_02!, headlights_01!, headlights_02!, headlights_01!, headlights_02!, headlights_01!, headlights_01!]
+        
+        let steamCloud_01 = UIImage(named: "steamCloud01")
+        let steamCloud_02 = UIImage(named: "steamCloud02")
+        steamImages = [steamCloud_01!, steamCloud_02!, steamCloud_01!, steamCloud_02!, steamCloud_01!, steamCloud_02!, steamCloud_01!, steamCloud_02!, steamCloud_01!]
+
     }
     
     @IBAction func startAnimation() {
@@ -55,6 +62,11 @@ class BeetleViewController: UIViewController {
         headlightsAnimation.duration = 0.5
         headlightsAnimation.repeatCount = 20
 
+        let steamAnimation = CAKeyframeAnimation(keyPath: "contents")
+        steamAnimation.calculationMode = kCAAnimationDiscrete
+        steamAnimation.values = steamImages.map {$0.cgImage as AnyObject}
+        steamAnimation.duration = 0.8
+        steamAnimation.repeatCount = 12.5
         
         UIView.animate(withDuration: 4,
                        delay: 0,
@@ -70,6 +82,7 @@ class BeetleViewController: UIViewController {
                         self.backWheelReflectionView.shake(values: [0, 1, 0, 1.5, 0], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.backWheelReflectionView, duration: 0.75, animationDuration: self.animationDuration)
                         self.frontWheelView.shake(values: [0, 1.5, 0, 1, 0], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.frontWheelView, duration: 0.75, animationDuration: self.animationDuration)
                         self.frontWheelReflectionView.shake(values: [0, 1.5, 0, 1, 0], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.frontWheelReflectionView, duration: 0.75, animationDuration: self.animationDuration)
+                        self.steamView.shake(values: [0.75, -0.75, 0.75, -0.75, 0.75], keyTimes: [0, 0.25, 0.5, 0.75, 1], animatedImageView: self.steamView, duration: 0.75, animationDuration: self.animationDuration)
                         
                         
                         //Wheels
@@ -80,6 +93,10 @@ class BeetleViewController: UIViewController {
                         
                         //Headlights Shine
                         self.headlightsView.layer.add(headlightsAnimation, forKey: "contents")
+                        
+                        //Steam Animation
+                        self.steamView.layer.add(steamAnimation, forKey: "contents")
+                        
                         
                         //Road
                         self.roadBoxView.moveTheRoad(road1View: self.road1View, road2View: self.road2View, duration: 1, animationDuration: self.animationDuration)
