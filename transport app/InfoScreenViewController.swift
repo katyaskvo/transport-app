e38671413cc68928cad033b69442675d578d35fe
katyaskvo <katyaskvo@gmail.com>
2017-07-17 15:21:55 +0300

    show icon animations when view did load and place them into the container view

--- transport app/InfoScreenViewController.swift
+++ transport app/InfoScreenViewController.swift
@@ -3,43 +3,43 @@ import Foundation
 import Lottie
 
 class InfoScreenViewController: UIViewController {
-    
+    @IBOutlet var iconMomBabyView: UIImageView!
+    @IBOutlet weak var iconCauseEffectView: UIImageView!
+    @IBOutlet weak var iconHandEyeView: UIImageView!
+    @IBOutlet weak var iconFineMotorView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
-    }
-
-    @IBAction func animateIcon(_ sender: Any) {
-
-        let iconSize = 150
+        let iconSize = 120
         
+        let animationMomBabyView = LOTAnimationView(name: "icon_momBaby")
+        animationMomBabyView.frame = CGRect(x:0, y: 0, width: iconSize, height: iconSize)
+        animationMomBabyView.loopAnimation = true
+        animationMomBabyView.contentMode = .scaleAspectFill
+        self.iconMomBabyView.addSubview(animationMomBabyView)
+        animationMomBabyView.play()
+
         let animationCauseEffectView = LOTAnimationView(name: "icon_causeEffect")
-        animationCauseEffectView.frame = CGRect(x:350, y: 300, width: iconSize, height: iconSize)
-
+        animationCauseEffectView.frame = CGRect(x:0, y: 0, width: iconSize, height: iconSize)
+        animationCauseEffectView.loopAnimation = true
         animationCauseEffectView.contentMode = .scaleAspectFill
-        
+        self.iconCauseEffectView.addSubview(animationCauseEffectView)
+        animationCauseEffectView.play()
+
         let animationFineMotorView = LOTAnimationView(name: "icon_fineMotor")
-        animationFineMotorView.frame = CGRect(x:600, y: 300, width: iconSize, height: iconSize)
+        animationFineMotorView.frame = CGRect(x:0, y: 0, width: iconSize, height: iconSize)
+        animationFineMotorView.loopAnimation = true
         animationFineMotorView.contentMode = .scaleAspectFill
+        self.iconHandEyeView.addSubview(animationFineMotorView)
+        animationFineMotorView.play()
 
         let animationHandEyeView = LOTAnimationView(name: "icon_handEye")
-        animationHandEyeView.frame = CGRect(x:350, y: 600, width: iconSize, height: iconSize)
+        animationHandEyeView.frame = CGRect(x:0, y: 0, width: iconSize, height: iconSize)
+        animationHandEyeView.loopAnimation = true
         animationHandEyeView.contentMode = .scaleAspectFill
-
-        let animationMomBabyView = LOTAnimationView(name: "icon_momBaby")
-        animationMomBabyView.frame = CGRect(x:600, y: 600, width: iconSize, height: iconSize)
-        animationMomBabyView.contentMode = .scaleAspectFill
-
-        
-        self.view.addSubview(animationCauseEffectView)
-        self.view.addSubview(animationFineMotorView)
-        self.view.addSubview(animationHandEyeView)
-        self.view.addSubview(animationMomBabyView)
-
-        animationCauseEffectView.play()
-        animationFineMotorView.play()
+        self.iconFineMotorView.addSubview(animationHandEyeView)
         animationHandEyeView.play()
-        animationMomBabyView.play()
+
     }
+    
 }
